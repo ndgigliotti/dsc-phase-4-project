8dc1c3dcb403514c9bb9582d22f08b78257ab03f
ndgigliotti <ndgigliotti@gmail.com>
2021-06-18 02:16:16 -0400

    add POS tagging and lemmatizing

--- tools/language.py
+++ tools/language.py
@@ -3,21 +3,26 @@ import re
 import string
 from functools import partial, singledispatch
 from operator import itemgetter
-from typing import Callable
+from typing import Callable, DefaultDict, List, Tuple
 
 import gensim.parsing.preprocessing as gsp
+import nltk
 import numpy as np
+from pandas.core.dtypes.inference import is_nested_list_like
 from scipy.sparse import csr_matrix
 import pandas as pd
+from pandas.api.types import is_list_like
 from fuzzywuzzy.fuzz import WRatio as weighted_ratio
 from fuzzywuzzy.process import extractOne as extract_one
 from IPython.core.display import Markdown, display
 from pandas._typing import ArrayLike, AnyArrayLike
 from unidecode import unidecode
-from nltk.tokenize.casual import TweetTokenizer
-
+from nltk.corpus import wordnet
+from nltk.stem.wordnet import WordNetLemmatizer
+from os.path import normpath
 from .typing import RandomBasis, SeriesOrArray, StrOrPattern
 from . import utils
+from ._validation import _check_if_tagged
 
 # Vectorized preprocessing functions from Gensim
 strip_short = np.vectorize(gsp.strip_short, otypes=[str])
@@ -30,6 +35,47 @@ stem_text = np.vectorize(gsp.stem_text, otypes=[str])
 
 # Vectorized Unicode-to-readable-ASCII converter
 unidecode = np.vectorize(unidecode, otypes=[str])
+
+TREEBANK_TAGS = frozenset(
+    {
+        "CC",
+        "CD",
+        "DT",
+        "EX",
+        "FW",
+        "IN",
+        "JJ",
+        "JJR",
+        "JJS",
+        "LS",
+        "MD",
+        "NN",
+        "NNP",
+        "NNPS",
+        "NNS",
+        "PDT",
+        "POS",
+        "PRP",
+        "PRP$",
+        "RB",
+        "RBR",
+        "RBS",
+        "RP",
+        "SYM",
+        "TO",
+        "UH",
+        "VB",
+        "VBD",
+        "VBG",
+        "VBN",
+        "VBP",
+        "VBZ",
+        "WDT",
+        "WP",
+        "WP$",
+        "WRB",
+    }
+)
 
 
 def lowercase(docs: np.ndarray):
@@ -47,15 +93,6 @@ def strip_punct(docs: np.ndarray, repl=" ", punct=string.punctuation, exclude=""
     return docs.reshape(shape)
 
 
-def tweet_tokenize(text, preserve_case=True, reduce_len=False, strip_handles=False):
-    tokenizer = TweetTokenizer(
-        preserve_case=preserve_case,
-        reduce_len=reduce_len,
-        strip_handles=strip_handles,
-    )
-    return tokenizer.tokenize(text)
-
-
 def readable_sample(
     data: pd.Series, n: int = 10, random_state: RandomBasis = None
 ) -> None:
@@ -63,6 +100,78 @@ def readable_sample(
         raise ValueError(f"Expected Series, got {type(data)}")
     data = data.sample(n=n, random_state=random_state)
     display(Markdown(data.to_markdown()))
+
+
+def treebank_pos_to_wordnet(pos):
+    """Inspired by https://stackoverflow.com/questions/15586721"""
+    wordnet_pos = dict(
+        J=wordnet.ADJ,
+        V=wordnet.VERB,
+        N=wordnet.NOUN,
+        R=wordnet.ADV,
+    )
+    return wordnet_pos.get(pos[0].upper(), wordnet.NOUN)
+
+
+def tokenize_tag(docs: pd.Series, tokenizer: Callable = None):
+    if tokenizer is None:
+        docs = docs.str.split()
+    else:
+        docs = docs.map(tokenizer)
+    docs = docs.map(nltk.pos_tag)
+    return docs
+
+
+@singledispatch
+def wordnet_lemmatize(docs: pd.Series, tokenizer: Callable = None):
+    # Check if docs are POS tagged
+    pretagged = _check_if_tagged(docs)
+
+    # Tokenize and tag POS if not tagged
+    if not pretagged:
+        docs = tokenize_tag(docs, tokenizer=tokenizer)
+
+    # Convert Treebank tags to Wordnet tags
+    words = docs.explode().dropna()
+    treebank_pos = words.map(itemgetter(1))
+    wordnet_pos = treebank_pos.map(treebank_pos_to_wordnet)
+    words = words.map(itemgetter(0))
+    words = pd.Series(list(zip(words, wordnet_pos)), index=words.index)
+
+    # Lemmatize
+    wnl = WordNetLemmatizer()
+    words = words.map(lambda x: wnl.lemmatize(*x))
+
+    # Restore tags if docs were pretagged
+    if pretagged:
+        words = pd.Series(list(zip(words, treebank_pos)), index=words.index)
+    return utils.implode(words).rename(docs.name).reindex_like(docs)
+
+
+@wordnet_lemmatize.register
+def _(docs: np.ndarray, tokenizer: Callable = None):
+    shape = docs.shape
+    docs = wordnet_lemmatize(pd.Series(docs.flat), tokenizer=tokenizer)
+    return docs.to_numpy().reshape(shape)
+
+
+def filter_pos(docs: pd.Series, include:List=None, exclude:List=None, tokenizer: Callable = None, as_tokens=False):
+    if include is not None and exclude is not None:
+        raise ValueError("Must pass only one: `include` or `exclude`")
+    elif include is None and exclude is None:
+        return docs
+    elif include is None and exclude is not None:
+        keep = set(TREEBANK_TAGS) - set(exclude)
+    else:
+        keep = set(include)
+    tagged = tokenize_tag(docs, tokenizer=tokenizer)
+    words = utils.expand(tagged.explode().dropna(), labels=["word", "tag"])
+    words = words.loc[words.tag.isin(keep), "word"]
+    words = utils.implode(words)
+    if not as_tokens:
+        words = words.str.join(" ")
+    docs = words.rename(docs.name).reindex_like(docs)
+    return docs
 
 
 def locate_patterns(
