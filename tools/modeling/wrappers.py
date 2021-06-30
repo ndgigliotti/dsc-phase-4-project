ba6a80346f87fef294717b96ea6190f5ef539a2e
ndgigliotti <ndgigliotti@gmail.com>
2021-06-30 06:19:15 -0400

    simplify wrapper

--- tools/modeling/wrappers.py
+++ tools/modeling/wrappers.py
@@ -1,46 +1,15 @@
-from functools import singledispatch, singledispatchmethod
-from typing import Any, Union
-import numpy as np
+from functools import singledispatchmethod
+from typing import Union
 
-import pandas as pd
-from scipy.sparse import csr_matrix
+from numpy import ndarray
+from pandas.core.frame import DataFrame
+from pandas.core.series import Series
+from scipy.sparse import spmatrix
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import _VectorizerMixin
 from sklearn.utils.validation import check_is_fitted
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
 
-from .. import utils
 from .._validation import _validate_transformer
-from ..language import frame_doc_vecs
-
-
-# @singledispatch
-# def _wrap(obj: BaseEstimator):
-#     if obj is None or isinstance(obj, str):
-#         pass
-#     elif not isinstance(obj, PandasWrapper):
-#         obj = PandasWrapper(obj)
-#     return obj
-
-
-# @_wrap.register
-# def _(obj: Pipeline):
-#     new_pipe = [(x, _wrap(y)) for x, y in obj.steps]
-#     return Pipeline(new_pipe, memory=obj.memory, verbose=obj.verbose)
-
-
-# @_wrap.register
-# def _(obj: ColumnTransformer):
-#     new_ct = [(x, _wrap(y), z) for x, y, z in obj.transformers_]
-#     return ColumnTransformer(
-#         new_ct,
-#         remainder=obj.remainder,
-#         sparse_threshold=obj.sparse_threshold,
-#         n_jobs=obj.n_jobs,
-#         transformer_weights=obj.transformer_weights,
-#         verbose=obj.verbose,
-#     )
+from ..typing import NDStruct
 
 
 class PandasWrapper(BaseEstimator, TransformerMixin):
@@ -51,45 +20,28 @@ class PandasWrapper(BaseEstimator, TransformerMixin):
             _validate_transformer(self.transformer)
 
     @property
-    def is_vectorizer(self):
-        if self.transformer is None:
-            result = False
-        elif isinstance(self.transformer, _VectorizerMixin):
-            result = True
-        elif "vectorizer" in self.transformer.__class__.__name__.lower():
-            result = True
-        elif "raw_documents" in utils.get_param_names(self.transformer.fit):
-            result = True
-        else:
-            result = False
-        return result
-
-    @property
     def passthrough(self):
         return self.transformer is None or self.transformer == "passthrough"
 
     def unwrap(self):
         return self.transformer
 
-    def _reconst_frame(self, X: Union[np.ndarray, csr_matrix]):
-        if isinstance(X, csr_matrix):
+    def _reconst_frame(self, X: Union[ndarray, spmatrix]):
+        if isinstance(X, spmatrix):
             X = X.todense()
-        X = pd.DataFrame(X, self.index_, self.columns_)
+        X = DataFrame(X, self.index_, self.columns_)
         for column, dtype in self.dtypes_.items():
             X[column] = X[column].astype(dtype)
         return X
 
-    def _vocab_frame(self, X: Union[np.ndarray, csr_matrix]):
-        if isinstance(X, csr_matrix):
-            X = frame_doc_vecs(X, self.transformer.vocabulary_, self.index_)
-        else:
-            raise NotImplementedError()
-        return X
-
     @singledispatchmethod
-    def fit(self, X: pd.DataFrame, y: pd.Series = None, **fit_params):
-        if self.is_vectorizer:
-            raise TypeError("Cannot pass DataFrame to vectorizer")
+    def fit(self, X: NDStruct, y: NDStruct = None, **fit_params):
+        if not self.passthrough:
+            self.transformer.fit(X, y, **fit_params)
+        return self
+
+    @fit.register
+    def _(self, X: DataFrame, y: Series = None, **fit_params):
         self.index_ = X.index
         self.columns_ = X.columns
         self.dtypes_ = X.dtypes
@@ -100,66 +52,51 @@ class PandasWrapper(BaseEstimator, TransformerMixin):
         return self
 
     @fit.register
-    def _(self, X: pd.Series, y: pd.Series = None, **fit_params):
-        if self.is_vectorizer and not self.passthrough:
-            self.index_ = X.index
-            self.transformer.fit(X.to_list(), y, **fit_params)
-        else:
+    def _(self, X: Series, y: Series = None, **fit_params):
+        if not self.passthrough:
             self.fit(X.to_frame(), y, **fit_params)
         return self
 
-    def _transform_preserve_struct(self, X: pd.DataFrame):
-        init_shape = X.shape
-        X = self.transformer.transform(X.to_numpy())
-        if X.shape != init_shape:
-            raise RuntimeError("Transformation must preserve shape and order")
-        return self._reconst_frame(X)
-
-    def _itransform_preserve_struct(self, X: pd.DataFrame):
-        init_shape = X.shape
-        X = self.transformer.inverse_transform(X.to_numpy())
-        if X.shape != init_shape:
-            raise RuntimeError("Inverse transformation must preserve shape and order")
-        return self._reconst_frame(X)
-
     @singledispatchmethod
-    def transform(self, X: pd.DataFrame):
+    def transform(self, X: NDStruct):
         check_is_fitted(self)
-        if self.is_vectorizer:
-            raise TypeError("Cannot pass DataFrame to vectorizer")
         if not self.passthrough:
-            X = self._transform_preserve_struct(X)
+            X = self.transformer.transform(X)
         return X
 
     @transform.register
-    def _(self, X: pd.Series):
+    def _(self, X: DataFrame):
         check_is_fitted(self)
         if not self.passthrough:
-            if self.is_vectorizer:
-                X = self.transformer.transform(X.to_list())
-                X = self._vocab_frame(X)
-            else:
-                X = self._transform_preserve_struct(X.to_frame()).squeeze()
+            init_shape = X.shape
+            X = self.transformer.transform(X.to_numpy())
+            if X.shape != init_shape:
+                raise RuntimeError("Transformation must preserve shape")
+            X = self._reconst_frame(X)
         return X
 
+    @transform.register
+    def _(self, X: Series):
+        return self.transform(X.to_frame()).squeeze()
+
     @singledispatchmethod
-    def inverse_transform(self, X: pd.DataFrame):
+    def inverse_transform(self, X: NDStruct):
         check_is_fitted(self)
-        if self.is_vectorizer:
-            raise NotImplementedError(
-                "Inverse transform not implemented for vectorizers"
-            )
         if not self.passthrough:
-            X = self._itransform_preserve_struct(X)
+            X = self.transformer.inverse_transform(X)
         return X
 
     @inverse_transform.register
-    def _(self, X: pd.Series):
+    def _(self, X: DataFrame):
         check_is_fitted(self)
-        if self.is_vectorizer:
-            raise NotImplementedError(
-                "Inverse transform not implemented for vectorizers"
-            )
         if not self.passthrough:
-            X = self._itransform_preserve_struct(X.to_frame()).squeeze()
+            init_shape = X.shape
+            X = self.transformer.inverse_transform(X.to_numpy())
+            if X.shape != init_shape:
+                raise RuntimeError("Inverse transformation must preserve shape")
+            X = self._reconst_frame(X)
         return X
+
+    @inverse_transform.register
+    def _(self, X: Series):
+        return self.inverse_transform(X.to_frame()).squeeze()
